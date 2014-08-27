830c2c633722d1fca93dcbdb516c478f0117403b
hudson <hudson-watchers@newrelic.com>
2014-08-27 08:08:47 -0700

    Updates Azure Cloud Services NuGet package to version 3.6.177.0

--- nuget-test/tests/package_content.Tests.ps1
+++ nuget-test/tests/package_content.Tests.ps1
@@ -15,7 +15,7 @@ $modulesPath = (Split-Path -parent $MyInvocation.MyCommand.Path).Replace("\tests
 Import-Module (Join-Path $modulesPath global_config.psm1) -Force
 
     Describe "package_content" {
-		$agentVersion = "3.5.107.0"
+		$agentVersion = "3.6.177.0"
 		$serverMonitorVersion = "3.2.6.0"
 		
 		Context "When package exists" {
