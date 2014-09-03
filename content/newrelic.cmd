e3f24f293725ef942b4004406a5da02db697978d
hudson <hudson-watchers@newrelic.com>
2014-09-02 19:39:15 -0700

    Updates Azure Cloud Services NuGet package to version 3.7.32.0

--- content/newrelic.cmd
+++ content/newrelic.cmd
@@ -24,7 +24,7 @@ IF %NR_ERROR_LEVEL% EQU 0 (
 	ECHO %ldt% : Begin installing the New Relic .net Agent >> "%RoleRoot%\nr.log" 2>&1
 
 	:: Current version of the installer
-	SET NR_INSTALLER_NAME=NewRelicAgent_x64_3.6.177.0.msi
+	SET NR_INSTALLER_NAME=NewRelicAgent_x64_3.7.32.0.msi
 	:: Path used for custom configuration and worker role environment varibles
 	SET NR_HOME=%ALLUSERSPROFILE%\New Relic\.NET Agent\
 
