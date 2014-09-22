42b96a64379785b839936e635f1c85fa00f56f03
hudson <hudson-watchers@newrelic.com>
2014-09-22 14:01:25 -0700

    Updates Azure Cloud Services NuGet package to version 3.7.121.0

--- tools/install.ps1
+++ tools/install.ps1
@@ -2,7 +2,7 @@ param($installPath, $toolsPath, $package, $project)
 
 Import-Module (Join-Path $toolsPath NewRelicHelper.psm1)
 
-$newRelicAgentMsiFileName = "NewRelicAgent_x64_3.7.103.0.msi"
+$newRelicAgentMsiFileName = "NewRelicAgent_x64_3.7.121.0.msi"
 $newRelicServerMonitorMsiFileName = "NewRelicServerMonitor_x64_3.2.6.0.msi"
 
 Write-Host "***Updating project items newrelic.cmd, $newRelicAgentMsiFileName, and $newRelicServerMonitorMsiFileName***"
