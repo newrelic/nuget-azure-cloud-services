8cb03aced56cfbcba584825345fd011e0fce5ff1
Micah Zoltu <micah@zoltu.net>
2014-12-17 16:00:51 -0800

    Remove some comments that encouraged changing of this file.
    Changing this file will break NuGet package updates, so it is discouraged.  Rather than putting comments that suggest it, it is better to just not say anything.  If a user wants to change behavior, they should think very hard if they want to personally maintain that behavior.

--- content/newrelic.cmd
+++ content/newrelic.cmd
@@ -5,10 +5,7 @@ set ldt=%ldt:~0,4%-%ldt:~4,2%-%ldt:~6,2% %ldt:~8,2%:%ldt:~10,2%:%ldt:~12,6%
 
 SET NR_ERROR_LEVEL=0
 
-:: Comment out the line below if you do not want to install the New Relic Agent
 CALL:INSTALL_NEWRELIC_AGENT
-
-:: Comment out the line below if you do not want to install the New Relic Windows Server Monitor
 CALL:INSTALL_NEWRELIC_SERVER_MONITOR
 
 IF %NR_ERROR_LEVEL% EQU 0 (
@@ -35,12 +32,6 @@ IF %NR_ERROR_LEVEL% EQU 0 (
 	) ELSE (
 	    msiexec.exe /i %NR_INSTALLER_NAME% /norestart /quiet NR_LICENSE_KEY=%LICENSE_KEY% /lv* %RoleRoot%\nr_install.log
 	)
-
-	:: CUSTOM newrelic.xml : Uncomment the line below if you want to copy a custom newrelic.xml file into your instance
-	REM copy /Y "newrelic.xml" "%NR_HOME%" >> %RoleRoot%\nr.log
-
-	:: CUSTOM INSTRUMENTATION : Uncomment the line below to copy custom instrumentation into the agent directory.
-	REM copy /Y "CustomInstrumentation.xml" "%NR_HOME%\extensions" >> %RoleRoot%\nr.log
 
 	:: WEB ROLES : Restart the service to pick up the new environment variables
 	:: 	if we are in a Worker Role then there is no need to restart W3SVC _or_
@@ -88,7 +79,3 @@ GOTO:EOF
 	)
 
 GOTO:EOF
-
-
-
-
