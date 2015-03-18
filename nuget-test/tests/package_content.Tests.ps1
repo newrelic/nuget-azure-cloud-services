99d6a9c91a4af24fea8554fb50623c2bb212b74e
hudson <hudson-watchers@newrelic.com>
2015-03-17 21:11:33 -0700

    Updates NewRelicWindowsAzure to version 4.1.136.0

--- nuget-test/tests/package_content.Tests.ps1
+++ nuget-test/tests/package_content.Tests.ps1
@@ -15,7 +15,7 @@ $modulesPath = (Split-Path -parent $MyInvocation.MyCommand.Path).Replace("\tests
 Import-Module (Join-Path $modulesPath global_config.psm1) -Force
 
     Describe "package_content" {
-		$agentVersion = "4.1.134.0"
+		$agentVersion = "4.1.136.0"
 		$serverMonitorVersion = "3.3.3.0"
 		
 		Context "When package exists" {
